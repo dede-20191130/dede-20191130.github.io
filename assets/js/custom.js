d1fc0ff945b36097338659d7e113fef17d5795d8
dede_20191130 <1044adad@gmail.com>
2021-09-12 23:11:08 +0900

    correct page anchor jumping

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,14 @@
-// 切替可能GIFを含むimg要素の設定
 window.addEventListener("DOMContentLoaded", (event) => {
+    // 切替可能GIFを含むimg要素の設定
     const images = document.querySelectorAll('img.togglable-gif-image');
     images.forEach((image) => {
         image.addEventListener('click', toggleGifImage);
+    });
+    // ページ内アンカージャンプ時
+    // ヘッダ部分によるスクロールのずれのための補正
+    const pageAnchors = document.querySelectorAll('a[href^="#"]');
+    pageAnchors.forEach((anchor) => {
+        anchor.addEventListener('click', correntScroll);
     });
 
 })
@@ -16,4 +22,27 @@ function toggleGifImage() {
     image.setAttribute('data-before', src);
 
     image.src = before ? before : src.substr(0, src.lastIndexOf(".")) + ".gif";
+}
+
+// ページ内アンカージャンプ時
+// ヘッダ部分によるスクロールのずれのための補正
+// ref:https://senoweb.jp/note/fixheader-anchorlink/
+function correntScroll(event) {
+    // 対象アンカー
+    const href = event.currentTarget.getAttribute("href");
+    if (href === "#" || href === "") {
+        return;
+    }
+    // ヘッダ高さ
+    const hdH = document.querySelector("header")?.clientHeight || 0;
+    // ジャンプ先要素のdocumentに対する位置（Y）の取得
+    const target = document.querySelector(href);
+    const positionY = window.pageYOffset + target.getBoundingClientRect().top - hdH;
+    // スクロールを指定
+    window.scrollTo({
+        top: positionY,
+    })
+
+    event.preventDefault();
+
 }
